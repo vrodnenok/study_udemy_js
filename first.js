40326212075e2d3e4c2309d913ad40b3695ff7e0
Victor Rodnenok <victor.r@ukr.net>
2022-03-16 11:09:15 +0200

    first commit

---
+++ first.js
@@ -0,0 +1,10 @@
+/* jshint -W097 */
+
+let max = 'help';
+console.log(max);
+
+let object1 = {
+    a: 100
+};
+
+console.log(object1.a);
---
+++ second.es6
@@ -0,0 +1,4 @@
+/*jshint esversion: 6*/
+
+let amp = prompt("are you sure?", "47");
+console.log(amp);
